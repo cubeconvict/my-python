cd0f2246bee9b01f31846e9b8cc603a85e367a6a
CubeConvict <cubeconvict@server.fake>
2012-12-13 15:31:41 -0800

    Modifying to take command line input

--- line-up.py
+++ line-up.py
@@ -1,12 +1,17 @@
 #Little script for taking in a csv file and adding spaces in order to line up all the commas
 
+#import sys so that the file to be lined up can be passed in as a command line argument
+import os, sys
+
 from array import *
 
 myline = 'a,b,cd,efg,h'
 myline2 = 'asdfgasg,asdgadsghgaha,asdghbasdg'
 longestitem = 0
 
-inputlist_data = (myline,myline2)
+#inputlist_data = (myline,myline2)
+#inputlist_data = os.path.dirname(sys.argv[0])
+
 
 def most_items(myinput):
     #intended to step through all of the lines in an array and find which line has the largest number of items and return the 
@@ -18,15 +23,17 @@ def most_items(myinput):
     return mostitems
     
 def count_lengths(thelist):
-    arraylengths = []
+    #create an list that specifies the length of each item in the list passedin as input
+    listlengths = []
     for item in thelist:
         arraylengths.append(len(item))
     return arraylengths
 
 def send_numbers(inputstuff):
-    myarray = inputstuff.split(',')
-    mylenarray = count_lengths(myarray)
-    return (myarray, mylenarray)
+    #checks the length of each item in a list 
+    mylist = inputstuff.split(',')
+    mylenlist = count_lengths(mylist)
+    return (mylist, mylenlist)
 
 def find_longest(inputlist, indexer):
     longest = 0
@@ -39,11 +46,25 @@ def find_longest(inputlist, indexer):
                 pass
     return longest
 
-#find the number if items in the longest line
-myarray = (myline.split(','),myline2.split(','))
+def splitFile(inputfile):
+  route = []
+  for line in open(inputfile, 'r'):
+    route.append(line.strip().split('>')) 
 
+  return route
+
+#pull in the file from the command line and assign it to an array where each line is an item
+myarray = splitFile(sys.argv[1])
+print (myarray)
+#find the number if items in the longest line
+largestnumberofitems = most_items(myarray)
+print ("The line with the most items is #" + str(largestnumberofitems))
 #find the largest number of characters at each index
+
+
 #step through all indices using a try/while loop
+
+    
 
 
 
