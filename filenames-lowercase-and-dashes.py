5c89732e4908bebbf2bec289c0a35a696be224ca
CubeConvict <kraft.toby@gmail.com>
2018-04-25 10:54:23 -0700

    Adding a little bit to replace & with and

--- filenames-lowercase-and-dashes.py
+++ filenames-lowercase-and-dashes.py
@@ -1,4 +1,6 @@
-'simple script for removing spaces, replacing them with dashes, and making a filename all lowercase
+"""simple script for removing spaces, 
+replacing them with dashes, 
+and making a filename all lowercase"""
 
 
 import glob, re, os
@@ -8,6 +10,7 @@ for filename in glob.glob('*.xlsx' ):
 	new_name = filename.lower()
 	new_name = new_name.replace(" ", "-") 
 	new_name = new_name.replace("--", "-")
+	new_name = new_name.replace("&", "and")
 	print(filename, ' -> ', new_name)
 	os.rename(filename, new_name)
 
